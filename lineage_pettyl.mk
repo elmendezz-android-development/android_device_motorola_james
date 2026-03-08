464b65a76aea5faffda672bdce2be48f6ff4c71d
elmendezz <165435968+elmendezz@users.noreply.github.com>
2026-03-08 10:05:26 -0700

    Update device identifiers for pettyl

--- lineage_pettyl.mk
+++ lineage_pettyl.mk
@@ -14,10 +14,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_james
-PRODUCT_DEVICE := james
+PRODUCT_NAME := lineage_pettyl
+PRODUCT_DEVICE := pettyl
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto e5 play
+PRODUCT_MODEL := moto e5 play go
 PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
@@ -25,9 +25,9 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.product.name
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="james_a-user 8.0.0 OCPS27.91-157-20 22 release-keys" \
-    PRODUCT_NAME="james"
+    PRIVATE_BUILD_DESC="pettyl-user 8.1.0 OCPS27.91-157-20 22 release-keys" \
+    PRODUCT_NAME="pettyl"
 
-BUILD_FINGERPRINT := motorola/james_a/james:8.0.0/OCPS27.91-157-20/22:user/release-keys
+BUILD_FINGERPRINT := motorola/pettyl/pettyl:8.1.0/OCPS27.91-157-20/22:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
